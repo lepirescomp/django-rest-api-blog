4eb9f1d5b95a215308c21a70aae6940d71192ec8
caosao <leticiaFlorentinoPires>
2020-01-01 19:24:58 -0300

    commit leticia views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework import generics
 # Create your views here.
+
+from .models import Post
+from .serializers import PostSerializers
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
