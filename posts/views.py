1d235f64b2242155a9c8b10f6571797eca0508ac
caosao <leticiaFlorentinoPires>
2020-01-03 16:22:18 -0300

    permissions leticia

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics
-# Create your views here.
 
 from .models import Post
 from .serializers import PostSerializers
+from .permissions import IsAuthorOrReadOnly
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializers
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializers
